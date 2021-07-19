11ece2f36fffc092fc740683421ff5d30bc26a97
Mayore <mayorerobert@gmail.com>
2021-07-19 23:51:50 +0300

    added health & readiness routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello', function () {
     return 'Hello, World!';
 });
+
+Route::get('/health', function () {
+    return 'ok';
+});
+
+Route::get('/readiness', function () {
+    return 'ok';
+});
